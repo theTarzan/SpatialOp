5249bbeb183b8be13abef866ab401727bb420578
Kevin Goulding <kevingoulding@gmail.com>
2012-02-03 10:23:52 -0700

    second commit

---
+++ C2F.py
@@ -0,0 +1,8 @@
+def C2F():
+	Celsius		= input('Enter degrees in Celsius: ');
+	Fahrenheit = (9.0 / 5.0) * Celsius +32;
+	print Celsius, 'Celsius =', Fahrenheit, 'Fahrenheit';
+	
+C2F()
+
+# run this code in Terminal using menu in TextWrangler
